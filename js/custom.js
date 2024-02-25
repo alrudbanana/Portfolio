45388a93a9b4feca8c5737770188f4d0ba7f7d30
migyeongbanana <pratt3343@gmail.com>
2024-02-26 00:37:58 +0900

    ⚡️ 백앤드 포트폴리오 추가

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,11 @@ $('.web .portfolio-menu p').on('click',function(){
     $('.portfolio-content .portfolioWrap').removeClass('on');
     $('.portfolio-content .portfolioWrap:eq('+ index +')').addClass('on');
 })
+
+$('.web .portfolio-menu-clone p').on('click',function(){
+    let index = $('.web .portfolio-menu-clone p').index(this);
+    $('.web .portfolio-menu-clone p').removeClass('on');
+    $('.web .portfolio-menu-clone p:eq('+ index +')').addClass('on');
+    $('.portfolio-content .portfolioWrapClone').removeClass('on');
+    $('.portfolio-content .portfolioWrapClone:eq('+ index +')').addClass('on');
+})
